35acc59ffeeeec0ed1736866a99747d1b18c0e54
yoofyoof <67996486+yoofyoof@users.noreply.github.com>
2022-07-24 10:19:03 -0700

    create new profile activity and copy over the changes

---
+++ app/src/main/java/edu/neu/madcourse/wellness_studio/Profile_With_Local_DB_Example.java
@@ -0,0 +1,14 @@
+package edu.neu.madcourse.wellness_studio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Profile_With_Local_DB_Example extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_with_local_db_example);
+    }
+}
