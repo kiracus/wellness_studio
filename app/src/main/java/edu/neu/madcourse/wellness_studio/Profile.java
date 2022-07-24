a354e9dd6c954367cb19f61fe0e19222f4d31634
yoofyoof <67996486+yoofyoof@users.noreply.github.com>
2022-07-24 10:53:28 -0700

    create profile class

---
+++ app/src/main/java/edu/neu/madcourse/wellness_studio/Profile.java
@@ -0,0 +1,14 @@
+package edu.neu.madcourse.wellness_studio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
