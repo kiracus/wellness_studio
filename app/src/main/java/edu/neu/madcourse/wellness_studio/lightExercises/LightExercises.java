03310af3166e415400c675544fbaf12c3b4c7b06
yoofyoof <67996486+yoofyoof@users.noreply.github.com>
2022-07-25 22:57:20 -0700

    create alarm notification and implemented set and cancel alarm with the on/off switch

--- app/src/main/java/edu/neu/madcourse/wellness_studio/lightExercises/LightExercises.java
+++ app/src/main/java/edu/neu/madcourse/wellness_studio/lightExercises/LightExercises.java
@@ -1,11 +1,20 @@
 package edu.neu.madcourse.wellness_studio.lightExercises;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.AlarmManager;
 import android.app.AlertDialog;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.AlarmClock;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
@@ -20,6 +29,8 @@ import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import java.sql.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.Locale;
 
@@ -30,20 +41,22 @@ import localDatabase.lightExercise.LightExercise;
 
 public class LightExercises extends AppCompatActivity {
     AppDatabase appDatabase;
-    int hour, min;
+    int hour, min = -1;
     TextView timeTextView;
     Switch reminderSwitch;
+    PendingIntent pendingIntent;
+    AlarmManager alarmManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_light_exercises);
+        createNotificationChannel();
 
         View view = findViewById(R.id.activity_light_exercises);
         timeTextView = findViewById(R.id.timeTextView);
         reminderSwitch = findViewById(R.id.reminderSwitch);
 
-        popTimePicker(view);
         setReminderSwitch(reminderSwitch);
 
         appDatabase = AppDatabase.getDbInstance(this.getApplicationContext());
@@ -61,6 +74,19 @@ public class LightExercises extends AppCompatActivity {
             updateLightExerciseInfo(lightExercise);
         }
         loadLightExerciseInfo();
+    }
+
+    private void createNotificationChannel() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "lightExerciseAndroidChannel";
+            String description = "Channel for light exercise alarm";
+            int important = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel notificationChannel = new NotificationChannel("lightExerciseAndroid",name,important);
+            notificationChannel.setDescription(description);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
     }
 
 
@@ -83,7 +109,7 @@ public class LightExercises extends AppCompatActivity {
 
         int style = AlertDialog.THEME_HOLO_LIGHT;
 
-        TimePickerDialog timePickerDialog = new TimePickerDialog(this,style,onTimeSetListener,hour,min,false);
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this,style,onTimeSetListener,hour,min,true);
         timePickerDialog.setTitle("Select Time");
         timePickerDialog.show();
     }
@@ -94,14 +120,50 @@ public class LightExercises extends AppCompatActivity {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(isChecked) {
-                    Toast.makeText(getApplicationContext(),"reminder is on",Toast.LENGTH_SHORT).show();
+                    //alarm is never set
+                    if(hour == -1) {
+                        Toast.makeText(getApplicationContext(), "please set an alarm", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    setAlarm();
                 }
-                else{
-                    Toast.makeText(getApplicationContext(),"reminder is off",Toast.LENGTH_SHORT).show();
+                else {
+                    cancelAlarm();
                 }
             }
         });
     }
+
+    // a slight couple mins delay between setted time and actual notification time is expected.
+    public void setAlarm() {
+        Intent intent = new Intent(this,AlarmReceiver.class);
+        long millis = convertHourAndMinToMilliSeconds();
+
+        alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        pendingIntent = PendingIntent.getBroadcast(this,0,intent,0);
+        Log.d("myApp","milis" + millis);
+        alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP,millis,AlarmManager.INTERVAL_DAY,pendingIntent);
+        Toast.makeText(getApplicationContext(),"reminder is on",Toast.LENGTH_SHORT).show();
+    }
+
+    public void cancelAlarm() {
+        Intent intent = new Intent(this,AlarmReceiver.class);
+        pendingIntent = PendingIntent.getBroadcast(this,0,intent,0);
+        if(alarmManager == null) {
+            alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        }
+        alarmManager.cancel(pendingIntent);
+        Toast.makeText(getApplicationContext(),"reminder is off",Toast.LENGTH_SHORT).show();
+    }
+
+    private long convertHourAndMinToMilliSeconds() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR,hour,min);
+        Log.d("myApp","calenar" + calendar.getTime());
+        long millis = calendar.getTimeInMillis();
+        return millis;
+    }
+
     public void createNewLightExercise(LightExercise lightExercise) {
         appDatabase.lightExerciseDao().insertLightExercise(lightExercise);
     }
