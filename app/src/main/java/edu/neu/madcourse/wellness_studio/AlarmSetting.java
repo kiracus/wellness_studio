82a69a40e076415af21fa9f699ccb116053dd934
simin-zhang <siminzhang94@gmail.com>
2022-07-29 00:21:24 -0700

    update wakeup sleep goal

---
+++ app/src/main/java/edu/neu/madcourse/wellness_studio/AlarmSetting.java
@@ -0,0 +1,14 @@
+package edu.neu.madcourse.wellness_studio;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AlarmSetting extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_alarm_time);
+    }
+}
