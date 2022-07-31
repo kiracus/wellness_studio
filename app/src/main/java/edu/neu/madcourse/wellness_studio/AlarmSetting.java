4899c72279b13972aba9dc9b1ec0eb3830a31159
simin-zhang <siminzhang94@gmail.com>
2022-07-31 02:52:10 -0700

    update alarm setting

--- app/src/main/java/edu/neu/madcourse/wellness_studio/AlarmSetting.java
+++ app/src/main/java/edu/neu/madcourse/wellness_studio/AlarmSetting.java
@@ -1,14 +1,112 @@
 package edu.neu.madcourse.wellness_studio;
 
+import android.app.AlarmManager;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.NumberPicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.sql.Time;
+
+import edu.neu.madcourse.wellness_studio.leaderboard.Leaderboard;
+import edu.neu.madcourse.wellness_studio.lightExercises.LightExercises;
+
 public class AlarmSetting extends AppCompatActivity {
+
+    TimePicker sleepTimePicker, wakeupTimePicker;
+    static String sleepAlarmHour, sleepAlarmMin, wakeupAlarmHour, wakeupAlarmMin;
+    Button saveButton;
+    ImageButton homeBtn, exerciseBtn, sleepBtn, leaderboardBtn;
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_alarm_time);
+
+        sleepTimePicker = (TimePicker) findViewById(R.id.sleep_timePicker);
+        sleepTimePicker.setIs24HourView(true);
+
+        wakeupTimePicker = (TimePicker) findViewById(R.id.wakeup_timePicker);
+        wakeupTimePicker.setIs24HourView(true);
+
+        saveButton = findViewById(R.id.change_save_btn);
+
+
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(AlarmSetting.this, "save the changes", Toast.LENGTH_SHORT).show();
+                saveChanges();
+                Intent intent = new Intent(AlarmSetting.this, WakeupSleepGoal.class);
+
+            }
+        });
+
+
+        homeBtn = findViewById(R.id.imageButton_home);
+        exerciseBtn = findViewById(R.id.imageButton_exercise);
+        sleepBtn = findViewById(R.id.imageButton_sleep);
+        leaderboardBtn = findViewById(R.id.imageButton_leaderboard);
+        homeBtn.setOnClickListener(v -> startActivity(new Intent(AlarmSetting.this, Greeting.class)));
+
+        // set click listeners for buttons
+        exerciseBtn.setOnClickListener(v -> goToLightExercise());
+        //exerciseGoBtn.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, LightExercises.class)));
+        sleepBtn.setOnClickListener(v -> startActivity(new Intent(AlarmSetting.this, WakeupSleepGoal.class)));
+        leaderboardBtn.setOnClickListener(v -> startActivity(new Intent(AlarmSetting.this, Leaderboard.class)));
+
+
     }
+
+
+    private void goToLightExercise() {
+        startActivity(new Intent(AlarmSetting.this, LightExercises.class));
+    }
+
+    public void getCurrentSleepAlarm() {
+        sleepTimePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+                sleepAlarmHour = hourOfDay + "";
+                sleepAlarmMin = minute + "";
+
+            }
+        });
+
+    }
+
+    public void getCurrentWakeupAlarm() {
+        wakeupTimePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+                wakeupAlarmHour = hourOfDay + "";
+                wakeupAlarmMin = minute + "";
+            }
+        });
+
+
+    }
+
+    public void saveChanges() {
+        getCurrentSleepAlarm();
+        getCurrentWakeupAlarm();
+
+    }
+
+
+
+
+
+
 }
--- app/src/main/java/edu/neu/madcourse/wellness_studio/WakeupSleepGoal.java
+++ app/src/main/java/edu/neu/madcourse/wellness_studio/WakeupSleepGoal.java
@@ -4,15 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import java.util.Calendar;
+
+import edu.neu.madcourse.wellness_studio.leaderboard.Leaderboard;
+import edu.neu.madcourse.wellness_studio.lightExercises.LightExercises;
+
 
 public class WakeupSleepGoal extends AppCompatActivity {
-    Button alarmSettingBtn, sleepAlarmOnOffBtn, wakeupAlarmOnOffBtn;
-    TextView sleepAlarmTV, wakeupAlarmTV, sleepHoursTV;
+    Button sleepAlarmOnOffBtn, wakeupAlarmOnOffBtn;
+    TextView sleepAlarmTV, wakeupAlarmTV, sleepHoursTV, sleepAlarmOnTV, wakeupAlarmOnTV;
     ImageView profile, sleepAlarmSetting, wakeupAlarmSetting;
+    ImageButton homeBtn, exerciseBtn, sleepBtn, leaderboardBtn;
+    String sleepAlarmOnOffCheck = "ALARM OFF", wakeAlarmOnOffCheck = "ALARM OFF";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,27 +44,97 @@ public class WakeupSleepGoal extends AppCompatActivity {
         sleepAlarmSetting = findViewById(R.id.setting_dot);
         wakeupAlarmSetting = findViewById(R.id.wakeup_setting_dot);
 
+        sleepAlarmOnTV = findViewById(R.id.alarm_on_TV);
+        wakeupAlarmOnTV = findViewById(R.id.wakeup_alarm_on_TV);
+
+
         sleepAlarmSetting.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openSettingActivity();
+                Intent intent = new Intent(WakeupSleepGoal.this, AlarmSetting.class);
+                startActivity(intent);
             }
         });
 
         wakeupAlarmSetting.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openSettingActivity();
+                Intent intent = new Intent(WakeupSleepGoal.this, AlarmSetting.class);
+                startActivity(intent);
+            }
+        });
+
+        sleepAlarmOnOffBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onSleepToggleClicked(v);
+            }
+        });
+
+        wakeupAlarmOnOffBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onWakeupToggleClicked(v);
+            }
+        });
+
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(WakeupSleepGoal.this, Profile.class));
             }
         });
 
 
 
+        homeBtn = findViewById(R.id.imageButton_home);
+        exerciseBtn = findViewById(R.id.imageButton_exercise);
+        sleepBtn = findViewById(R.id.imageButton_sleep);
+        leaderboardBtn = findViewById(R.id.imageButton_leaderboard);
+        homeBtn.setOnClickListener(v -> startActivity(new Intent(WakeupSleepGoal.this, Greeting.class)));
+
+        // set click listeners for buttons
+        exerciseBtn.setOnClickListener(v -> goToLightExercise());
+        //exerciseGoBtn.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, LightExercises.class)));
+        sleepBtn.setOnClickListener(v -> startActivity(new Intent(WakeupSleepGoal.this, WakeupSleepGoal.class)));
+        leaderboardBtn.setOnClickListener(v -> startActivity(new Intent(WakeupSleepGoal.this, Leaderboard.class)));
+
 
 
     }
 
-    private void openSettingActivity() {
-        startActivity(new Intent(WakeupSleepGoal.this, AlarmSetting.class));
+    private void goToLightExercise() {
+        startActivity(new Intent(WakeupSleepGoal.this, LightExercises.class));
     }
+
+    private void onSleepToggleClicked(View view) {
+        boolean on = ((ToggleButton)view).isChecked();
+
+        if (on) {
+            sleepAlarmOnTV.setText("ALARM ON");
+            sleepAlarmOnOffCheck = "ALARM ON";
+        } else {
+            sleepAlarmOnTV.setText("ALARM OFF");
+            sleepAlarmOnOffCheck = "ALARM OFF";
+        }
+    }
+
+    private void onWakeupToggleClicked(View view) {
+        boolean on = ((ToggleButton)view).isChecked();
+
+        if (on) {
+            wakeupAlarmOnTV.setText("ALARM ON");
+            wakeAlarmOnOffCheck = "ALARM ON";
+
+        } else {
+            wakeupAlarmOnTV.setText("ALARM OFF");
+            wakeAlarmOnOffCheck = "ALARM OFF";
+        }
+    }
+
+    public void updateAlarmTextView() {
+                wakeupAlarmTV.setText(AlarmSetting.wakeupAlarmHour + ":" + AlarmSetting.wakeupAlarmMin);
+                sleepAlarmTV.setText(AlarmSetting.sleepAlarmHour + ":" + AlarmSetting.sleepAlarmMin);
+    }
+
 }
